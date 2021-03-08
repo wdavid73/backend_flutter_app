86ba29e9354b5bfac04bf6e7c6c936504fb9cf90
Wilson Padilla <wpadilla4@cuc.edu.co>
2021-03-08 16:45:26 -0500

    revision de urls para el frontend y agregado un paquete para autenticacion con las apps

--- api_admin/Table/urls.py
+++ api_admin/Table/urls.py
@@ -4,7 +4,7 @@ from .View.list_tables_by_restaurant import list_tables_by_restaurant
 
 urlpatterns = [
     path("", GetAndPost.as_view(), name="get_and_post"),
-    path("tables_by_restaurant/", list_tables_by_restaurant,
+    path("tables_by_restaurant", list_tables_by_restaurant,
          name="tables_by_restaurant")
 
 ]
