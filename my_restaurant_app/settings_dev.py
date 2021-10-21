796f5cec7611ffa7a10f61fa7a4f4876fd0d8f34
Wilson Padilla <wdavidw26@gmail.com>
2021-10-21 09:00:17 -0500

    enable get without authentication

--- my_restaurant_app/settings_dev.py
+++ my_restaurant_app/settings_dev.py
@@ -126,12 +126,13 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    #    'rest_framework.authentication.BasicAuthentication',
-    #    'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.permissions.AllowAny',
+        #    'rest_framework.authentication.BasicAuthentication',
+        #    'rest_framework.authentication.SessionAuthentication',
     ],
-    #'DEFAULT_PERMISSION_CLASSES': [
+    # 'DEFAULT_PERMISSION_CLASSES': [
     #    'rest_framework.permissions.IsAuthenticated',
-    #]
+    # ]
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
