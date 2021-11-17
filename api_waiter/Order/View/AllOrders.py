608e9e8175c411955b42ac477450c82e0b05267c
Wilson Padilla <wdavidw26@gmail.com>
2021-11-17 15:22:47 -0500

    new endpoint fix method post to get

--- api_waiter/Order/View/AllOrders.py
+++ api_waiter/Order/View/AllOrders.py
@@ -2,14 +2,15 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.decorators import api_view,permission_classes
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from my_restaurant_app.validations import user_validate_required
 from my_restaurant_app.customPermissions import TokenPermission
 from ..Model.ModelOrder import Order
 from ..Serializer.SerializerOrder import OrderSerializer
 
-@api_view(["POST"])
+
+@api_view(["GET"])
 @user_validate_required
 @permission_classes([TokenPermission])
 def all_orders(request: Request):
