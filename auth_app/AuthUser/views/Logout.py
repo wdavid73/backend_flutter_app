3f07a14721ade27d36ad70e1b82df41f0df909b8
Wilson Padilla <wdavidw26@gmail.com>
2021-12-21 14:01:44 -0500

    handle error token not provider in logount

--- auth_app/AuthUser/views/Logout.py
+++ auth_app/AuthUser/views/Logout.py
@@ -11,12 +11,16 @@ class Logout(APIView):
     authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = (permissions.AllowAny,)
 
-    def get(self, request, format=None):
+    """ def get(self, request, format=None):
         request.user.auth_token.delete()
         logout(request)
-        return Response({"data": "You are Logout", "status": status.HTTP_200_OK})
+        return Response({"data": "You are Logout", "status": status.HTTP_200_OK}) """
 
     def post(self, request):
-        token = Token.objects.get(key=request.data)
-        token.delete()
-        return Response({"data": "You are Logout", "status": status.HTTP_200_OK})
+        try:
+            token = Token.objects.get(key=request.data)
+            token.delete()
+            return Response({"data": "You are Logout"}, status= status.HTTP_200_OK)
+        except Token.DoesNotExist as e:
+            return Response({"error" : str(e)} , status=status.HTTP_400_BAD_REQUEST)
+
